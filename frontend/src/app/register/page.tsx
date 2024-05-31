d6e7f5b4125b807250853b28a11e1532e055c7dc
fewdw <frederic.alefebvre@gmail.com>
2024-05-30 20:17:28 -0400

    redirect register popup added

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,10 +1,22 @@
+'use client';
 import React from "react";
 import NotLoggedNavBar from "../components/NavBar";
 import RegisterForm from "../components/RegisterForm";
+import { useRouter } from 'next/navigation';
+import Popup from "../components/Popup";
+
 
 const page = () => {
+
+  const router = useRouter();
+  const { searchParams } = new URL(window.location.href);
+  const register = searchParams.get('register');
+
   return (
+
     <div>
+      {register === 'true' && <Popup></Popup>}
+
       <RegisterForm></RegisterForm>
     </div>
   );
