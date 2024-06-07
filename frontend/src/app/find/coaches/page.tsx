ac6db0b813d58f4280aedf34e6c7e213706ec821
fewdw <frederic.alefebvre@gmail.com>
2024-06-07 15:29:21 -0400

    view coaches page

--- frontend/src/app/find/coaches/page.tsx
+++ frontend/src/app/find/coaches/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+'use client';
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation"; // Correct hook for App Router
+import Cookies from "js-cookie";
+import {jwtDecode} from "jwt-decode";
+import MenuBar from "@/app/components/MenuBar";
 
-const page = () => {
-  return <div></div>;
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const jwt = Cookies.get("jwt");
+    if (jwt) {
+      const { account_type } = jwtDecode(jwt);
+      if (account_type !== "boxer" && account_type !== "coach") {
+        router.push('/');
+      }
+    } else {
+      router.push('/');
+    }
+  }, [router]);
+
+  return (
+    <div>
+      <MenuBar></MenuBar>
+      
+    </div>
+  );
 };
 
-export default page;
+export default Page;
