14a2f835f591d31bef4a2e90d0f2c2198b45fb99
fewdw <frederic.alefebvre@gmail.com>
2024-06-09 14:35:58 -0400

    styling for boxer profile

--- frontend/src/app/find/boxers/boxer/[boxer_id]/page.tsx
+++ frontend/src/app/find/boxers/boxer/[boxer_id]/page.tsx
@@ -26,38 +26,50 @@ const BoxerPage = () => {
         <div>
             <MenuBar />
             {boxer ? (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>UUID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Birth Date</th>
-                            <th>Country</th>
-                            <th>Gender</th>
-                            <th>Gym ID</th>
-                            <th>Level</th>
-                            <th>Number of Fights</th>
-                            <th>Stance</th>
-                            <th>Weight</th>
-                        </tr>
-                    </thead>
-                    <tbody>ç
-                        <tr>
-                            <td>{boxer.UUID}</td>
-                            <td>{boxer.fname}</td>
-                            <td>{boxer.lname}</td>
-                            <td>{boxer.birth_date}</td>
-                            <td>{boxer.country}</td>
-                            <td>{boxer.gender}</td>
-                            <td>{boxer.gym_id}</td>
-                            <td>{boxer.level}</td>
-                            <td>{boxer.num_of_fights}</td>
-                            <td>{boxer.stance}</td>
-                            <td>{boxer.weight}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="flow-root rounded-lg border border-gray-100 py-3 shadow-sm m-8">
+                    <dl className="-my-3 divide-y divide-gray-100 text-sm">
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">First Name</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.fname}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Last Name</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.lname}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Birth Date</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.birth_date}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Country</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.country}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Gender</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.gender}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Gym ID</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.gym_id}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Level</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.level}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Number of Fights</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.num_of_fights}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Stance</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.stance}</dd>
+                        </div>
+                        <div className="grid grid-cols-1 gap-1 p-3 sm:grid-cols-3 sm:gap-4">
+                            <dt className="font-medium text-gray-900">Weight</dt>
+                            <dd className="text-gray-700 sm:col-span-2">{boxer.weight}</dd>
+                        </div>
+                    </dl>
+                </div>
             ) : (
                 <p>Loading...</p>
             )}
