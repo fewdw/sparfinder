52f2212111077e09f12aa3228de77443d6f859c6
fewdw <frederic.alefebvre@gmail.com>
2024-06-13 11:46:33 -0400

    env for the frontend api route

--- frontend/src/app/utils/apiConfig.ts
+++ frontend/src/app/utils/apiConfig.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const CREATE_ACCOUNT_URL = `${BASE_URL}/sparfinder/api/user/register`;
 export const LOGIN_URL = `${BASE_URL}/sparfinder/api/user/login`;
 export const GET_COACH_URL = `${BASE_URL}/sparfinder/api/coach/profile/info`;
